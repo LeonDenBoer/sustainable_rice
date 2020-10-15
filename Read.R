d3669a99f526a19bd0fb122e8ff4e858ffd962b1
leondenboer <Boerleo@gmail.com>
2020-10-15 10:51:51 +0200

    reading columns

--- Read.R
+++ Read.R
@@ -1,9 +1,19 @@
 install.packages("readxl")
 # Loading
 library("readxl")
+library(tidyverse)
 
 # xlsx files
 spain <- read_excel("ES-2019-SRP-ASSESMENT.xlsx")
 india <- read_excel("ID-2019-SRP-ASSESMENT.xlsx")
 pakistan <- read_excel("PK_2019_SRP_ASSESMENT.xlsx")
 cambodia <- read_excel("KH_2019_SRP_ASSESMENT.xlsx")
+
+india %>% rename(
+  district = Tehsil,
+  stad = Block,
+  gemeente = Panchayat
+)
+
+
+
