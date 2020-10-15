16bceec08745fd8f86f9fdcb4ee2658940c998e0
leondenboer <Boerleo@gmail.com>
2020-10-15 10:06:41 +0200

    Reading

---
+++ Read.R
@@ -0,0 +1,9 @@
+install.packages("readxl")
+# Loading
+library("readxl")
+
+# xlsx files
+spain <- read_excel("ES-2019-SRP-ASSESMENT.xlsx")
+india <- read_excel("ID-2019-SRP-ASSESMENT.xlsx")
+pakistan <- read_excel("PK_2019_SRP_ASSESMENT.xlsx")
+cambodia <- read_excel("KH_2019_SRP_ASSESMENT.xlsx")
